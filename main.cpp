75abf2733e8fd78b7ce4bea845bc8e48eb0a31d1
miyukikdokdo <miyuki_ziyad@hotmail.com>
2015-11-11 18:42:40 +0100

    ziyad

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <map> //
+#include <set> //
+#include <sys/stat.h> //to get the size of a file
 
-int main(){
+using namespace std;
+struct stat results; 
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2) 
+	{
+		cout << "Il n'y a qu'un paramètre" << endl;
+		return 1;
+	}
+	
+	ifstream finput(argv[1]);
+	if (finput.is_open())
+	{
+		cout << "Lecture en cours de la database" << endl;
+		
+		finput.close();
+	}
+	else
+	{
+		cout << "Impossible d'ouvrir la database"<< endl;
+		return 1;
+	} 
 	
 	return 0;
 }
