ed3e64ca756d39aacf0e8a4e497d7f95e7413699
miyukikdokdo <miyuki_ziyad@hotmail.com>
2015-11-11 22:22:43 +0100

    ziyad

---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map> //
+#include <set> //
+#include <sys/stat.h> //to get the size of a file
+
+using namespace std;
+struct stat results; 
+
+class db_info
+{
+public:
+	string title;
+	string basename;
+	string time;
+	
+	long seqcount;
+	long maxseq;
+	long rcount;
+	
+	string taxid_filename;//
+	ifstream taxid_file; //
+	
+	long offset_phr;//
+	long offset_psq;//
+	db_info();
+	long getnames(); //
+};
+
+db_info::db_info()
+{
+	string title = "";
+	string basename = "";
+	string time = "";
+	
+	long seqcount = 0;
+	long maxseq = 0;
+	long rcount = 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+	//read a file into memory
+	if (argc < 2) 
+	{
+		cout << "Il n'y a qu'un paramètre" << endl;
+		return 1;
+	}
+	
+	ifstream idb(argv[1], ios::in | ios::binary); //mode binaire 
+	
+	if (idb) {
+		//get length to have the file into the buffer
+		idb.seekg(0,idb.end); //move to the end
+		int length = idb.tellg(); //tellg() is the current position
+		idb.seekg(0,idb.beg); //move to the beg
+		
+		char* buffer = new char[length]; 
+		
+		cout << "Reading " << length << " characters..." << endl;
+		
+		
+		//read data as a block
+		
+		idb.read(buffer,length); 
+		
+		if (idb)
+			cout << "success" << endl;
+		else 
+			cout << "error : only" << idb.gcount() << " could be read";
+		idb.close();
+	
+		delete[] buffer;
+	}
+	
+	return 0;
+}
