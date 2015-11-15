3c8ec4a26f9afa71505945bbc4aed8ba9471a510
miyukikdokdo <miyuki_ziyad@hotmail.com>
2015-11-15 04:49:55 +0100

    ziyad

--- main.cpp
+++ main.cpp
@@ -39,17 +39,8 @@ db_info::db_info()
 	long rcount = 0;
 }
 
-void query_init(const char * queryname, long symtype, long strands)
-{
-	if (argc < 3) 
-	{
-		cout << "Il n'y a que deux paramètre" << endl;
-		return 1;
-	}
-	queryname = argv[2];
-	long qlength;
-	string qdescription;
-	//ifstream iquery 
+//void query_init(const char * queryname, long symtype, long strands)
+
 
 
 int main(int argc, char *argv[])
@@ -61,7 +52,8 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	
-
+	string basename = argv[1];
+	
 	ifstream idb(argv[1], ios::in | ios::binary); //mode binaire 
 	
 	if (idb) {
@@ -81,9 +73,19 @@ int main(int argc, char *argv[])
 		
 		/* Now that we have the entire file buffered, 
 		 * we can take a look at some binary information*/
-		for (int i = 0; i < 3000 ; i++)
-			fprintf("%i",buffer[i]) ;
 		
+		char psq_ascii[28] = {'-','A','B','C','D','E','F','G','H','I','K','L','M','N',
+			'P','Q','R','S','T','V','W','X','Y','Z','U','*','O','J'};
+		 
+		for (int i = 0; i < 3000 ; i++)
+		{
+			//printf("%x", buffer[i]);
+			//cout << (int)buffer[i];
+			//if (argv[1] == basename)
+				//cout << psq_ascii[(int)buffer[i]]; //imprime db_sequences
+			
+		}
+
 
 		if (idb)
 			cout << "success" << endl;
@@ -95,6 +97,47 @@ int main(int argc, char *argv[])
 		
 	}
 		
+	if (argc < 3) 
+	{
+		cout << "Il n'y a que deux paramètre" << endl;
+		return 1;
+	}
+	 
+
+	string query_name = argv[2];
+	int query_length;
+	string query_description;
+	
+	ifstream iquery(argv[2]);
+	if (iquery)
+	{
+		string description;
+		getline(iquery, description);
+		description = description.substr(1); //on retire le >
+		query_description = description; //QUERY_DESCRIPTION
+		
+		int beg_seq = iquery.tellg(); 
+		char c;
+		while (iquery >> noskipws >> c)
+			query_length++;
+			
+		/*iquery.seekg(0,iquery.end);
+		query_length = (int)iquery.tellg() - beg_seq; //QUERY LENGTH
+		
+		vector<char> query_sequence();
+		char c;
+		while (iquery >> c)
+			query_sequence.push_back(c);
+		query_length = sizeof(query_sequence);*/
+		
+		iquery.close();
+	}
+	
+	cout << "Query file name: "<<query_name<<endl;
+	cout << "Query length: "<<query_length<<" residues"<<endl;
+	cout << "Query description: " <<query_description<<endl;
+	
+	
 	
 	return 0;
 }
